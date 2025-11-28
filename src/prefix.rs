98adee19e1e1b78917d48b3613e3ccff8c15fc79
Jonathan McDonnell <jonathan.mcdonnell@kinsaleins.com>
2025-11-28 13:40:57 -0500

    add git integration

--- src/prefix.rs
+++ src/prefix.rs
@@ -2,10 +2,15 @@ use std::io::Write;
 use std::io::stdout;
 use colored::Colorize;
 use crate::dir::get_curr;
+use git2::Repository;
 
 /// This method prints text before the shell cursor
 /// Adds the current dir, replaces the home dir with '~'
 pub fn print(home_dir: &String) {
+    if Ok(repo) = Repository::open(".") {
+        println!("we in a repo!")
+    }
+
     let trail = "$".bright_black();
 
     let curr_dir = get_curr();
