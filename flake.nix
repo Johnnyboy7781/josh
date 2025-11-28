9df4161b80cb9f015481eb54194e333c6d505187
Jonathan McDonnell <jonathan.mcdonnell@kinsaleins.com>
2025-11-28 11:54:32 -0500

    add shell metadata

--- flake.nix
+++ flake.nix
@@ -1,29 +1,38 @@
 {
-  description = "A basic shell written in Rust";
+    description = "A basic shell written in Rust";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-  };
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+        flake-utils.url = "github:numtide/flake-utils";
+    };
 
-  outputs = { self, nixpkgs, flake-utils, ... }: 
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-        };
-      in
-      {
-        packages.josh = pkgs.rustPlatform.buildRustPackage {
-          pname = "josh";
-          version = "0.0.1";
-          src = ./.;
-          cargoLock = {
-            lockFile = ./Cargo.lock;
-          };
-        };
+    outputs = { self, nixpkgs, flake-utils, ... }: 
+        flake-utils.lib.eachDefaultSystem (system:
+            let
+                pkgs = import nixpkgs {
+                    inherit system;
+                };
+            in
+            {
+                packages.josh = pkgs.rustPlatform.buildRustPackage {
+                    pname = "josh";
+                    version = "0.0.1";
+                    src = ./.;
 
-        defaultPackage = self.packages.${system}.josh;
-      }
-    );
+                    cargoLock = {
+                        lockFile = ./Cargo.lock;
+                    };
+
+                    meta = with pkgs.lib; {
+                        description = "A basic shell written in Rust";
+                        license = licenses.mit;
+                        platforms = platforms.linux;
+                        mainProgram = "josh";
+                        shell = true;
+                    };
+                };
+
+                defaultPackage = self.packages.${system}.josh;
+            }
+        );
 }
